31f9cfe7f2c67698574065a90e4fd5a375aa4fe0
Bent0le <bentolethomsen@gmail.com>
2024-06-21 17:46:58 +0200

    Wir haben Nachbarn

--- Maze_Generation/src/core.jl
+++ Maze_Generation/src/core.jl
@@ -7,7 +7,11 @@ struct Node
     function Node(width::Int, height::Int, position::Tuple{Int, Int})
         pos = position
         neighbours = []
-        
+        pos[1]-1 >= 1 ? append!(nachbar, [(pos[1]-1,pos[2])]) : nothing
+        pos[1]+1 <= n ? append!(nachbar, [(pos[1]+1,pos[2])]) : nothing
+        pos[2]-1 >= 1 ? append!(nachbar, [(pos[1],pos[2]-1)]) : nothing
+        pos[2]+1 <= m ? append!(nachbar, [(pos[1],pos[2]+1)]) : nothing
+
         leftchild = nothing
         rightchild = nothing
         return new(pos, neighbours, leftchild, rightchild)
