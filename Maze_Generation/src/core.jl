1211cd2447a4b78c33ab962610b04efe1a19a8dd
Bent0le <bentolethomsen@gmail.com>
2024-07-01 18:24:45 +0200

    Update core.jl

--- Maze_Generation/src/core.jl
+++ Maze_Generation/src/core.jl
@@ -1,20 +1,26 @@
-struct Node
+mutable struct Node
     pos::Tuple{Int, Int}
-    neighbours::Vector{Tuple{Int, Int}}
-    leftchild::Union{Node, Nothing}
-    rightchild::Union{Node, Nothing}
+    conected::Union{Vector{Tuple{Int, Int}}, Nothing}
+    #leftchild::Union{Node, Nothing}
+    #rightchild::Union{Node, Nothing}
 
-    function Node(width::Int, height::Int, position::Tuple{Int, Int})
-        pos = position
-        neighbours = []
-        pos[1]-1 >= 1 ? append!(nachbar, [(pos[1]-1,pos[2])]) : nothing
-        pos[1]+1 <= n ? append!(nachbar, [(pos[1]+1,pos[2])]) : nothing
-        pos[2]-1 >= 1 ? append!(nachbar, [(pos[1],pos[2]-1)]) : nothing
-        pos[2]+1 <= m ? append!(nachbar, [(pos[1],pos[2]+1)]) : nothing
-
+    function Node(pos::Tuple{Int, Int})
         leftchild = nothing
         rightchild = nothing
-        return new(pos, neighbours, leftchild, rightchild)
+        return new(pos, [])#, leftchild, rightchild)
     end
+end
+
+#function to generate a list of neighbouring nodes that haven't been visited yet
+function neighbours(node::Node,visited::Vector{Tuple{Int,Int}},size::Tuple{Int,Int})::Vector{Node}
+    #println("illegal neighbours (visited): $visited")
+    width = size[1]
+    height = size[2]
+    neighbours::Vector{Node} = []
+    node.pos[1]-1 >= 1 && !((node.pos[1]-1,node.pos[2]) in visited) ? push!(neighbours, Node((node.pos[1]-1,node.pos[2]))) : nothing
+    node.pos[1]+1 <= width && !((node.pos[1]+1,node.pos[2]) in visited) ? push!(neighbours, Node((node.pos[1]+1,node.pos[2]))) : nothing
+    node.pos[2]-1 >= 1 && !((node.pos[1],node.pos[2]-1) in visited) ? push!(neighbours, Node((node.pos[1],node.pos[2]-1))) : nothing
+    node.pos[2]+1 <= height && !((node.pos[1],node.pos[2]+1) in visited) ? push!(neighbours, Node((node.pos[1],node.pos[2]+1))) : nothing
+    return neighbours
 end
 
