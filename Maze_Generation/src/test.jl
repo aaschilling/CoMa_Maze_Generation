40649cf4fb4e9f31b9f623c6bfefdd196c6fc5bf
aaschilling <73670486+aaschilling@users.noreply.github.com>
2024-07-06 22:27:22 +0200

    test for solve in test.jl

--- Maze_Generation/src/test.jl
+++ Maze_Generation/src/test.jl
@@ -17,17 +17,22 @@ include("MazeGeneration.jl")
         @test size(sn.new_frame, 2) == 12
         end 
     end
-    #=
+
     @testset "solve" begin
         test_maze = maze(10,10)
-        path = 
-        @test length(test_maze.nodes) == 100
+        solve(test_maze)
+        @test length(test_maze.path) <= 100
         cohesive::Bool = true
-        for node in test_maze
+        for i in range(2, 100)
+            first = test_maze.nodes[i-1]
+            second = test_maze.nodes[i]
+            !in(second.pos, first.conected) ? cohesive = false : nothing
             length(node.conected) < 1 ? cohesive = false : nothing
         end
         @test cohesive == true
+        has_duplicates = length(test_maze.path) != length(unique(test_maze.path))
+        @test has_duplicates == false
     end
-    =#
+    
 end
 
