c3686c8811c7b03f314e194a1eb6f3279ac626c2
grknlbsld <laurathalmann7@gmail.com>
2024-07-08 13:22:40 +0200

    Update visualize.jl

--- Maze_Generation/src/visualize.jl
+++ Maze_Generation/src/visualize.jl
@@ -4,27 +4,30 @@ mutable struct MazeViz
     matrix::Matrix{Char}  
 end
 
-function MazeViz(maze::Maze, height::Int, width::Int)
+function MazeViz(maze::Maze, width::Int, height::Int)
     #Matrix mit richtiger Größe und alle als 'x' erstellen
-    m = 2*height+1
     n = 2*width+1
-    value = 'x'
-    matrix = fill(value, m, n)
-    #Alle Matrixwerte die aus geradzahligen Ints bestehen werden von 'x' zu 'o' -> sie sind die Punkte im Labyrinth
-    for i in 1:m
-        for j in 1:n
+    m = 2*height+1
+    value = '+'
+    matrix = fill(value, n, m)
+    #Alle Matrixwerte die aus geradzahligen Ints bestehen werden von '+' zu 'o' -> sie sind die Punkte im Labyrinth
+    # !Ränder und Wände schöner machen in gleicher for Schleife!
+    for i in 2:n
+        for j in 1:m
             if i % 2 == 0 && j % 2 == 0
                 matrix[i,j] = 'o'
             end
         end
     end
-    #=Herausfinden wo Wände und wo conected, und je nachdem dann Wände rauslöschen
-    for i in 1:m
-        for j in 1:n
+    
+    #println(maze.nodes[4,1].conected)
+    #Herausfinden wo Wände und wo conected, und je nachdem dann Wände rauslöschen
+    for i in 1:n
+        for j in 1:m
             if matrix[i,j] == 'o'
-                i_durch_2::Int = i/2
-                j_durch_2::Int = j/2
-                nachbarn::Vector{Tuple{Int, Int}} = maze.nodes[i_durch_2, j_durch_2].conected
+                i_durch_2::Int = i/2                                                            #von width
+                j_durch_2::Int = j/2                                                            #von height
+                nachbarn::Vector{Tuple{Int, Int}} = maze.nodes[j_durch_2, i_durch_2].conected
                 if typeof(nachbarn) != Nothing
                     while length(nachbarn) >= 1
                         (x,y) = nachbarn[1]
@@ -48,7 +51,7 @@ function MazeViz(maze::Maze, height::Int, width::Int)
                 end
             end
         end
-    end=#
+    end
     return MazeViz(matrix)
 end
 
@@ -59,7 +62,7 @@ function print_maze(maze2::MazeViz)
 end
 
 
-test = maze(2,3)
+test = maze(2,4)
 println(test)
-d = MazeViz(test,2,3)
+d = MazeViz(test , 2 , 4)
 print_maze(d)
