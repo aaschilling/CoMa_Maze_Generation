217e4edc2e4120ea55692042cc005438f58a12b6
aaschilling <73670486+aaschilling@users.noreply.github.com>
2024-07-09 19:12:06 +0200

    Update test.jl

--- Maze_Generation/test.jl
+++ Maze_Generation/test.jl
@@ -2,30 +2,45 @@ include("maze.jl")
 using Test
 
 @testset "Maze" verbose=true begin
+    # Testlabyrinthe, einmal quadratisch, immer 100 Felder, quadratisch rechteckig.
+    test_mazes = [(10,10), (5, 20), (20, 5)]
+
+    #=Testen der MazeGeneration.
+    Es wird getested ob das Labyrinth die richtige Größe hat und ob die Nodes zusammenhängend sind, 
+    indem geschaut wird, dass das Feld in den Nodes die die Verbidnugn anzeigt nicht leer ist.
+    =#
     @testset "Maze Generation" begin
-        test_maze = maze(10,10)
-        @test length(test_maze.nodes) == 100
-        cohesive::Bool = true
-        for node in test_maze.nodes
-            length(node.conected) < 1 ? cohesive = false : nothing
+        for i in test_mazes
+            test_maze = maze(i[1],i[2])
+            @test length(test_maze.nodes) == i[1]*i[2]
+            cohesive::Bool = true
+            for node in test_maze.nodes
+                length(node.conected) < 1 ? cohesive = false : nothing
+            end
+            @test cohesive == true
         end
-        @test cohesive == true
     end
 
+    #=Testen der solve Funktion.
+    Es wird getestet, dass der Pfad nicht größer ist als die Anzahl der möglichen Nodes, 
+    dass die Nodes verbunden sind, für jede Node ab der zweiten im path geschaut wird, ob die Node in den Verbundenen der vorherigen enthalten ist
+    und der Pfad keine Umwege nimmt, indem auf Duplikate durch vergleich der Längen der Listen nach Anwendung der unique Funktion überprüft wird 
+    =#
     @testset "solve" begin
-        test_maze = maze(10,10)
-        solve(test_maze)
+        for i in test_mazes
+            test_maze = maze(i[1],i[2])
 
-        @test length(test_maze.path) <= 100
-        cohesive::Bool = true
-        for i in range(2, 100)
-            first = test_maze.nodes[i-1]
-            second = test_maze.nodes[i]
-            in(second.pos, first.conected) ? cohesive = false : nothing
-            length(node.conected) < 1 ? cohesive = false : nothing
+            @test length(test_maze.path) <= i[1]*i[2]
+            cohesive::Bool = true
+            for i in range(2, size(test_maze.path, 1))
+                first_node = test_maze.path[i-1]
+                second_node = test_maze.path[i]
+                length(first_node.conected) < 1 ?  cohesive = false : nothing
+                !in(second_node.pos, first_node.conected) ?  cohesive = false : nothing
+            end
+            @test cohesive == true
+            has_duplicates = length(test_maze.path) != length(unique(test_maze.path))
+            @test has_duplicates == false
         end
-        @test cohesive == true
-        has_duplicates = length(test_maze.path) != length(unique(test_maze.path))
-        @test has_duplicates == false
     end
 end
